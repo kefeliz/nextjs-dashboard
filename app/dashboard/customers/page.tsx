0a971761278188770403fc1d52dd514e2737341b
kefeliz <felizk03@thryv.com>
2025-08-21 11:43:54 -0400

    Add dashboard layout and pages with navigation links
    - Created CustomersPage and InvoicePage components.
    - Implemented a Layout component with a side navigation.
    - Updated RootLayout to include global styles and fonts.
    - Enhanced main Page with logo, welcome message, and images.
    - Added NavLinks component for side navigation with dynamic styling.
    - Introduced font styles and home module CSS for visual elements.

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function CustomersPage({}: Props) {
+  return <p>Customers Page</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function InvoicePage({}: Props) {
+  return <div>Invoice page</div>;
+}
