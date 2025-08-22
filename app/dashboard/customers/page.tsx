27b09a05a8520b422d4fee370fe80a60e740a9f1
kefeliz <felizk03@thryv.com>
2025-08-22 16:33:38 -0400

    Implement authentication system with NextAuth; add login page and related components

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
+import { CustomerTableSkeleton } from "@/app/ui/skeletons";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import { Metadata } from "next";
 
-type Props = {};
+export const metadata: Metadata = {
+  title: "Customers",
+};
 
-export default function CustomersPage({}: Props) {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <Suspense fallback={<CustomerTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
